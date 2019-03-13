b61a3491b22a4335f2c22b5df0193a1aca9ebca5
novinary <novina.dev@gmail.com>
2019-03-13 12:54:54 +0000

    Work on the basic UI

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            To Do List:
-          </p>
+          <p>To Do List:</p>
         </header>
+        <div>
+          <label>Add To Do</label>
+          <input type="text" value="test" />
+        </div>
+        <div className="todo-app container">
+
+        </div>
       </div>
     );
   }
