b5f031cc90daab59d3c4f22a1a497cbeb77c1d5d
novinary <novina.dev@gmail.com>
2019-03-13 12:47:46 +0000

    Use materialize library for CSS styling

--- todo/src/App.js
+++ todo/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-             To Do List:
+            To Do List:
           </p>
         </header>
       </div>
